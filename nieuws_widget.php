987df749712e1b48c7c98541cea12135411b03b2
Sotumney <sonyium@gmx.com>
2020-03-23 21:57:28 +0100

    Cloned Bas to me
    look at it lol

---
+++ nieuws_widget.php
@@ -0,0 +1,9 @@
+<iframe src="nieuws_widget_content.php"
+        style="border-radius: 15px"
+        name="News_widget"
+        scrolling="no"
+        frameborder="1"
+        height="80%"
+        width="600px"
+        allowfullscreen>
+</iframe>
