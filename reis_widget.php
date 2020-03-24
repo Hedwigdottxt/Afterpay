9240bdde0a20a66de29171fb2a856fab1e5bac92
basiecrazy <54987042+basiecrazy@users.noreply.github.com>
2020-03-24 12:01:32 +0100

    Bus_stop_widget 1.0
    werkende bus stop widget via drgl.nl

---
+++ reis_widget.php
@@ -0,0 +1,9 @@
+<iframe src="reis_widget_content.php"
+        style="border-radius: 15px"
+        name="9292_widget"
+        scrolling="no"
+        frameborder="1"
+        height="790px"
+        width="450px"
+        allowfullscreen>
+</iframe>
