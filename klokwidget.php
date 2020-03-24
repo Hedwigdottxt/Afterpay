fddd7404b38451cd014f65ca73a87a964f712674
basiecrazy <54987042+basiecrazy@users.noreply.github.com>
2020-03-24 20:58:51 +0100

    Update klokwidget.php

--- klokwidget.php
+++ klokwidget.php
@@ -2,7 +2,7 @@
         style="border-radius: 15px"
         name="klokwidget"
         scrolling="no"
-        frameborder="1"
+        frameborder="0"
         height="417px"
         width="417px"
         allowfullscreen>
