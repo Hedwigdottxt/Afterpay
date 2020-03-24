50a0e1a5951e0b95bc7389c688f4045a778c8b55
ehhfrank <frank2001@hotmail.nl>
2020-03-24 21:41:17 +0100

    joe
    index scherm

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<style>
+
+
+</style>
+
+<body>
+    <img id="indeximage" src="indextext.png"  class="center" />
+    <img id="logo" src="logo.png"  class="logo" />
+
+   
+
+</body>
+<button onclick="window.location.href = 'loginpage.php';">
+    
+
+</button>
+
+</html>
