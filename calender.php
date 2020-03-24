9432d9d46d88366dfa90833b70243952e171f7ed
basiecrazy <54987042+basiecrazy@users.noreply.github.com>
2020-03-24 13:23:58 +0100

    Calender_widget 0.9
    Werkende google calender, 
    Zal zelf moeten instellen met eigen agenda, daarom handleiding aanwezig met stappen
    
    Eventueel in een huisstijl iframe??

---
+++ calender.php
@@ -0,0 +1,11 @@
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23a7f3af&amp;ctz=Europe%2FAmsterdam&amp;color=%237986CB&amp;showTz=0&amp;showTitle=1&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;showPrint=0&amp;mode=AGENDA&amp;showDate=0&amp;title=Belangrijke%20Data&amp;
+
+src=YmFzaWVjcmF6eTE0QGdtYWlsLmNvbQ&amp;"
+
+
+        style="border:solid 1px #777"
+        width="500"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+</iframe>
