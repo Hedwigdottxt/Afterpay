b386bd86ef5fdb3725fe3d712f26a6dd773ba37d
Hedwig <hedwig.schoone@student.nhlstenden.com>
2020-03-24 21:17:40 +0100

    nog blanco orderaantallenwidget

---
+++ orderaantallen_widget.php
@@ -0,0 +1,9 @@
+<iframe src="orderaantallen_content.php"
+        style="border-radius: 15px"
+        name="Orderaantallen_widget"
+        scrolling="no"
+        frameborder="1"
+        height="200px"
+        width="500px"
+        allowfullscreen>
+</iframe>
