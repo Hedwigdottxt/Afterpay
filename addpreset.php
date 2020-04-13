d103d8c57841c3b0ac74b11ac1069638cf9ec155
basiecrazy <54987042+basiecrazy@users.noreply.github.com>
2020-04-13 14:48:10 +0200

    addpreset processing
    verwerking van maktin_presets.php

---
+++ addpreset.php
@@ -0,0 +1,43 @@
+<?php
+$nameErr = "";
+$name = "";
+$error = true;
+$host = "localhost";
+$databaseName = "afterpay";
+$databaseusername = "root";
+$databasepassword = "student";
+
+if (isset($_POST["presetname"])) {
+    $error = false;
+
+    if (empty($_POST["presetname"])) {
+        $nameErr = "Preset name is required";
+        $error = true;
+    } else {
+        $name = test_input($_POST["presetname"]);
+    }
+
+    if (!$error) {
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$databaseName", $databaseusername, $databasepassword);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO afterpay.presets (presetname, layout)
+            VALUES ('$name', '')";
+            echo "gelukt";
+            // use exec() because no results are returned
+            $conn->exec($sql);
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+
+        $conn = null;
+    }
+}
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
