e5e38f55a411878123451cbe76715eea8583b47d
basiecrazy <54987042+basiecrazy@users.noreply.github.com>
2020-04-13 15:02:07 +0200

    2-2layout

---
+++ 2-2 layout.php
@@ -0,0 +1,23 @@
+<style>
+    .wrapper {
+        margin-left: 0.5%;
+        display: grid;
+        grid-template-columns: 49.5% 49.5%;
+        grid-gap: 10px;
+        color: #444;
+        height: 85%;
+    }
+    .box {
+        background-color: #444;
+        color: #fff;
+        border-radius: 5px;
+        padding: 20px;
+        font-size: 150%;
+    }
+    </style>
+<div class="wrapper">
+    <div id ="box a" class="box a" ondrop="drop(event)" ondragover="allowDrop(event)" >+</div>
+    <div id ="box a" class="box b" ondrop="drop(event)" ondragover="allowDrop(event)" >+</div>
+    <div id ="box a" class="box d" ondrop="drop(event)" ondragover="allowDrop(event)" >+</div>
+    <div id ="box a" class="box c" ondrop="drop(event)" ondragover="allowDrop(event)" >+</div>
+</div>
