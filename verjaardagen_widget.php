868dc957acb4d60123253b287cc2135b2e127934
Hedwigdottxt <hedwig.schoone@student.nhlstenden.com>
2020-04-14 11:32:53 +0200

    Verjaardagenwidget_unfinished

---
+++ verjaardagen_widget.php
@@ -0,0 +1,9 @@
+<iframe src="verjaardagen_content.php"
+        style="border-radius: 15px"
+        name="verjaardagenwidget"
+        scrolling="no"
+        frameborder="0"
+        height="100px"
+        width="180px"
+        allowfullscreen>
+</iframe>
