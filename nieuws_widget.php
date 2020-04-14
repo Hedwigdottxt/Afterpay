a2c61730b15f7dace02ed18d828afb308edb6cde
Sotumney <sonyium@gmx.com>
2020-04-14 17:10:31 +0200

    emptying branch

--- nieuws_widget.php
+++
@@ -1,9 +0,0 @@
-<iframe src="nieuws_widget_content.php"
-        style="border-radius: 15px"
-        name="News_widget"
-        scrolling="no"
-        frameborder="1"
-        height="80%"
-        width="600px"
-        allowfullscreen>
-</iframe>
