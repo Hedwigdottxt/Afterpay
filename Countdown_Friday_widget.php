d5899f198861ced280873b6e0134e8d7d3d95e8c
Es0ku <46606826+Es0ku@users.noreply.github.com>
2020-04-14 11:49:26 +0200

    Countdown widget complete

---
+++ Countdown_Friday_widget.php
@@ -0,0 +1,9 @@
+<iframe src="Countdown_Friday_content.php"
+        style="border-radius: 15px"
+        name="News_widget"
+        scrolling="no"
+        frameborder="1"
+        height="200px"
+        width="500px"
+        allowfullscreen>
+</iframe>
